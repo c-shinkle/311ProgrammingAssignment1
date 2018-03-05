7be5ebf342b52474b9cd97fda4924830278b749c
Christian <cshinkle@iastate.edu>
2018-03-05 15:45:04 -0600

    First shot at VectorLength for HashStringSimilarity

--- 311pa1/src/HashStringSimilarity.java
+++ 311pa1/src/HashStringSimilarity.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  * @author Christian Shinkle
@@ -5,33 +7,73 @@
  * @author Benjamin Trettin
  */
 public class HashStringSimilarity {
-	float size1, size2;
-	float similarity;
-	HashTable table1,table2;
-	int sLength;
+	private float size1, size2;
+	private float similarity;
+	private HashTable table1,table2;
+	private int shingleLength;
+	private ArrayList<Integer> keys;
 	public HashStringSimilarity(String s1, String s2, int sLength) {
-		this.sLength=sLength;
-		int sizeS1, sizeS2;
-		sizeS1 = s1.length()-sLength+1;
-		sizeS2 = s2.length()-sLength+1;
-		table1 = new HashTable(sizeS1);
-		table2 = new HashTable(sizeS1);
-		
+		keys = new ArrayList<>();
+		shingleLength=sLength;
+		table1 = new HashTable(s1.length()-sLength+1);
+		table2 = new HashTable(s2.length()-sLength+1);
+		fillTable(table1, s1);
+		fillTable(table2, s2);
 	}
-	
+
 	public float lengthOfS1() {
-		String sample = this.s1;
-		double squaredval=0; 
-		return (float) java.lang.Math.sqrt(squaredval);
+		return length(true);
 	}
 	
+	
+
 	public float lengthOfS2() {
-		String sample = this.s1;
-		double squaredval=0;
-		return (float) java.lang.Math.sqrt(squaredval);
+		return length(false);
 	}
 	
 	public float similarity() {
 		return 0;
+	}
+	
+	private float length(boolean isS1) {
+		HashTable table = (isS1) ? table1 : table2;
+		double result = 0;
+		for (Integer key : keys) {
+			ArrayList<Tuple> bin = table.search(key);
+			boolean[] hasChecked = new boolean[bin.size()];
+			for (int i = 0; i < bin.size(); i++) {
+				if (!hasChecked[i]) {
+					String shingle = bin.get(i).getValue(); 
+					int count = 1;
+					hasChecked[i] = true;
+					for (int j = i+1; j < bin.size();j++) {
+						if (!hasChecked[j] && shingle.equals(bin.get(i).getValue())) {
+							hasChecked[j] = true;
+							count++;
+						}
+					}
+					count *= count;
+					result += count;
+				}
+			}
+		}
+		return (float) Math.sqrt(result);
+	}
+	
+	private void fillTable(HashTable table, String s) {
+		for (int i = 0; i < s.length()-shingleLength+1;i++) {
+			String sub = s.substring(i, i+shingleLength);
+			//
+			int result = 0;
+			for (int j = 0; j < shingleLength; j++)
+				result += sub.charAt(j);
+			if (!tableContains(table, result))
+				keys.add(result);
+			table.add(new Tuple(result, sub));
+		}
+	}
+	
+	private boolean tableContains(HashTable table, int result) {
+		return table.search(result).size()==0;
 	}
 }
