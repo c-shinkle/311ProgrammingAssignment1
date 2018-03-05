75f4e19dd71d57533fada9d639fbf4c24ad839b7
Benjamin Trettin <ben@benmac.student.iastate.edu>
2018-03-05 13:36:29 -0600

    constructor adds for HashStringSimilairty

--- 311pa1/src/HashStringSimilarity.java
+++ 311pa1/src/HashStringSimilarity.java
@@ -5,17 +5,30 @@
  * @author Benjamin Trettin
  */
 public class HashStringSimilarity {
-	
+	float size1, size2;
+	float similarity;
+	HashTable table1,table2;
+	int sLength;
 	public HashStringSimilarity(String s1, String s2, int sLength) {
+		this.sLength=sLength;
+		int sizeS1, sizeS2;
+		sizeS1 = s1.length()-sLength+1;
+		sizeS2 = s2.length()-sLength+1;
+		table1 = new HashTable(sizeS1);
+		table2 = new HashTable(sizeS1);
 		
 	}
 	
 	public float lengthOfS1() {
-		return 0;
+		String sample = this.s1;
+		double squaredval=0; 
+		return (float) java.lang.Math.sqrt(squaredval);
 	}
 	
 	public float lengthOfS2() {
-		return 0;
+		String sample = this.s1;
+		double squaredval=0;
+		return (float) java.lang.Math.sqrt(squaredval);
 	}
 	
 	public float similarity() {
